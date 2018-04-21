45ac91f9a23f4765cfb2f504eb9fe42dfa5e05e6
zhaocong6 <1140253608@qq.com>
2018-04-21 16:32:47 +0800

    Update Redis.php

--- src/Redis.php
+++ src/Redis.php
@@ -1,15 +1,10 @@
 <?php
-
 namespace Redis;
-
 use Predis\Client;
-
 class Redis
 {
-
     private static $config;
     private $redis;
-
     /**
      * Redis constructor.
      * @param array $config
@@ -17,10 +12,8 @@ class Redis
     public function __construct($config = [])
     {
         $config = self::getConfig($config);
-
         $this->redis = new Client($config);
     }
-
     /**
      * 实例化调用
      * @param $name
@@ -32,7 +25,6 @@ class Redis
         // TODO: Implement __call() method.
         return call_user_func_array([$this->redis, $name], $arguments);
     }
-
     /**
      * @param array $config
      * @return array
@@ -40,25 +32,21 @@ class Redis
     private static function getConfig($config = [])
     {
         if (self::$config) return self::$config;
-
         //判断是否是实例化传值
         if (!empty($config)) {
             return self::$config = $config;
         }
-
         //判断是否是tp框架
         if (defined('THINK_VERSION')){
             self::$config = C('redis');
         }
-
         //设置默认参数
         if (empty(self::$config)){
-            $config = [
+            self::$config = [
                 'host'  =>  '127.0.0.1',
                 'port'  =>  '6379'
             ];
         }
-
-        return self::$config = $config;
+        return self::$config;
     }
 }
